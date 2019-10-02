c7edc7083c4d533522ffc1a7a48a089916f6a7f5
karol <carolina.chamorro@securitasdirect.es>
2019-10-02 16:31:36 +0200

    Initial Commit

---
+++ goteras-reto/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  goteras-reto
+//
+//  Created by Carolina Chamorro on 02/10/2019.
+//  Copyright © 2019 Carolina Chamorro. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
