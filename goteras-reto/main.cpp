58ba99f240a6ad569f6702b28ebdf226da914ff1
karol <carolina.chamorro@securitasdirect.es>
2019-10-03 12:30:41 +0200

    feat: añadir problema de goteras

--- goteras-reto/main.cpp
+++ goteras-reto/main.cpp
@@ -1,15 +1,35 @@
+// Problema <216> <goteras>
 //
-//  main.cpp
-//  goteras-reto
+// https://www.aceptaelreto.com
 //
-//  Created by Carolina Chamorro on 02/10/2019.
-//  Copyright © 2019 Carolina Chamorro. All rights reserved.
-//
+// <Carolina Chamorro Saldaña>
+//---------------------------------------------------------
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+void casoDePrueba() {
+    int seconds, hours, min, sec;
+    cin >> seconds;
+    
+    hours = seconds / 3600;
+    min = ((seconds - hours*3600)/60);
+    sec = seconds % 60;
+    
+    printf("%02d:%02d:%02d\n", hours, min, sec);
+    
+} // casoDePrueba
+
+//---------------------------------------------------------
+
+int main() {
+    
+    unsigned int numCasos, i;
+    
+    cin >> numCasos;
+    for (i = 0; i < numCasos; ++i) {
+        casoDePrueba();
+    }
     return 0;
-}
+    
+} // main
